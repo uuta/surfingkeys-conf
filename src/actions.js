b8a00e5c91a3da39a2da07ca555259616e86a65e
uuta <nightrage.metal66@gmail.com>
2023-06-12 10:29:03 +0900

    Added speech.js to deal with Azure Speech

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,9 @@
 import ghReservedNames from "github-reserved-names";
-import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 
 import api from "./api.js";
 import priv from "./conf.priv.js";
 import util from "./util.js";
+import { configureSpeechSDK, requestToAzure } from "./actions/speech.js";
 
 const { tabOpenLink, Front, Hints, Normal, RUNTIME } = api;
 
@@ -1207,104 +1207,6 @@ actions.doi.getLink = (provider) => {
     return null;
   }
   return priv.doi_handler(doi);
-};
-
-/* Azure Speech resources */
-/* https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/get-started-text-to-speech?pivots=programming-language-javascript&tabs=macos%2Cterminal#synthesize-to-file-output */
-const configureSpeechSDK = () => {
-  const speechConfig = sdk.SpeechConfig.fromSubscription(
-    priv.keys.speech_key,
-    priv.keys.speech_region,
-  );
-  speechConfig.speechSynthesisVoiceName = "en-US-DavisNeural";
-
-  const pushStream = sdk.PullAudioOutputStream.create();
-
-  return {
-    synthesizer: new sdk.SpeechSynthesizer(
-      speechConfig,
-      sdk.AudioConfig.fromStreamOutput(pushStream),
-    ),
-    pushStream,
-  };
-};
-
-const readWithTimeout = (stream, buffer, timeout) =>
-  new Promise((resolve, reject) => {
-    const timer = setTimeout(() => {
-      reject(new Error("Read operation timed out"));
-    }, timeout);
-
-    stream
-      .read(buffer)
-      .then((bytesRead) => {
-        clearTimeout(timer);
-        resolve(bytesRead);
-      })
-      .catch((err) => {
-        clearTimeout(timer);
-        reject(err);
-      });
-  });
-
-const readAllFromStream = async (pushStream) => {
-  const dataBuffer = new ArrayBuffer(1024 * 10); // adjust size as needed
-  const receivedData = [];
-  let bytesRead;
-
-  /* eslint-disable no-await-in-loop */
-  try {
-    do {
-      bytesRead = await readWithTimeout(pushStream, dataBuffer, 5000); // 5 second timeout
-      receivedData.push(new Uint8Array(dataBuffer.slice(0, bytesRead)));
-    } while (bytesRead > 0);
-  } catch (err) {
-    console.error(`Error reading from stream: ${err}`);
-  }
-
-  return new Blob(receivedData, { type: "audio/wav" });
-};
-
-const requestToAzure = (synthesizer, pushStream) => {
-  if (navigator.clipboard) {
-    navigator.clipboard.readText().then((text) => {
-      if (text === "") {
-        console.error("Clipboard is empty");
-        return;
-      }
-
-      synthesizer.speakTextAsync(
-        text,
-        async (result) => {
-          if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-            console.log("synthesis finished.");
-
-            // Convert the push stream to a blob and create a URL for it
-            const audioBlob = await readAllFromStream(pushStream);
-            const audioUrl = URL.createObjectURL(audioBlob);
-
-            // Create a link to download the file
-            const downloadLink = document.createElement("a");
-            downloadLink.href = audioUrl;
-            downloadLink.download = `${text}.wav`;
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-            document.body.removeChild(downloadLink);
-          } else {
-            console.error(
-              `Speech synthesis canceled, ${result.errorDetails}\nDid you set the speech resource key and region values?`,
-            );
-          }
-          synthesizer.close();
-        },
-        (err) => {
-          console.trace(`err - ${err}`);
-          synthesizer.close();
-        },
-      );
-      console.log("Now synthesizing to speaker output.");
-    });
-  }
 };
 
 actions.textToSpeech = () => {
---
+++ src/actions/speech.js
@@ -0,0 +1,101 @@
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+import priv from "../conf.priv.js";
+
+const { Clipboard } = api;
+
+/* Azure Speech resources */
+/* https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/get-started-text-to-speech?pivots=programming-language-javascript&tabs=macos%2Cterminal#synthesize-to-file-output */
+export const configureSpeechSDK = () => {
+  const speechConfig = sdk.SpeechConfig.fromSubscription(
+    priv.keys.speech_key,
+    priv.keys.speech_region,
+  );
+  speechConfig.speechSynthesisVoiceName = "en-US-DavisNeural";
+
+  const pushStream = sdk.PullAudioOutputStream.create();
+
+  return {
+    synthesizer: new sdk.SpeechSynthesizer(
+      speechConfig,
+      sdk.AudioConfig.fromStreamOutput(pushStream),
+    ),
+    pushStream,
+  };
+};
+
+const readWithTimeout = (stream, buffer, timeout) =>
+  new Promise((resolve, reject) => {
+    const timer = setTimeout(() => {
+      reject(new Error("Read operation timed out"));
+    }, timeout);
+
+    stream
+      .read(buffer)
+      .then((bytesRead) => {
+        clearTimeout(timer);
+        resolve(bytesRead);
+      })
+      .catch((err) => {
+        clearTimeout(timer);
+        reject(err);
+      });
+  });
+
+const readAllFromStream = async (pushStream) => {
+  const dataBuffer = new ArrayBuffer(1024 * 10); // adjust size as needed
+  const receivedData = [];
+  let bytesRead;
+
+  /* eslint-disable no-await-in-loop */
+  try {
+    do {
+      bytesRead = await readWithTimeout(pushStream, dataBuffer, 5000); // 5 second timeout
+      receivedData.push(new Uint8Array(dataBuffer.slice(0, bytesRead)));
+    } while (bytesRead > 0);
+  } catch (err) {
+    console.error(`Error reading from stream: ${err}`);
+  }
+
+  return new Blob(receivedData, { type: "audio/wav" });
+};
+
+export const requestToAzure = (synthesizer, pushStream) => {
+  Clipboard.read((response) => {
+    const text = response.data;
+    if (text === "") {
+      console.error("Clipboard is empty");
+      return;
+    }
+
+    synthesizer.speakTextAsync(
+      text,
+      async (result) => {
+        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+          console.log("synthesis finished.");
+
+          // Convert the push stream to a blob and create a URL for it
+          const audioBlob = await readAllFromStream(pushStream);
+          const audioUrl = URL.createObjectURL(audioBlob);
+
+          // Create a link to download the file
+          const downloadLink = document.createElement("a");
+          downloadLink.href = audioUrl;
+          downloadLink.download = `${text}.wav`;
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+        } else {
+          console.error(
+            `Speech synthesis canceled, ${result.errorDetails}\nDid you set the speech resource key and region values?`,
+          );
+        }
+        synthesizer.close();
+      },
+      (err) => {
+        console.trace(`err - ${err}`);
+        synthesizer.close();
+      },
+    );
+    console.log("Now synthesizing to speaker output.");
+  });
+};
