e40ce78cad18a5b5351f6b533eb822d486d0f455
uuta <nightrage.metal66@gmail.com>
2023-06-25 20:50:18 +0900

    Shorten the second of timeout

--- src/actions/speech.js
+++ src/actions/speech.js
@@ -12,7 +12,7 @@ export const configureSpeechSDK = () => {
     priv.keys.speech_key,
     priv.keys.speech_region,
   );
-  speechConfig.speechSynthesisVoiceName = "en-US-DavisNeural";
+  speechConfig.speechSynthesisVoiceName = priv.keys.speech_voice;
 
   const pushStream = sdk.PullAudioOutputStream.create();
 
@@ -44,14 +44,14 @@ const readWithTimeout = (stream, buffer, timeout) =>
   });
 
 const readAllFromStream = async (pushStream) => {
-  const dataBuffer = new ArrayBuffer(1024 * 10); // adjust size as needed
+  const dataBuffer = new ArrayBuffer(1024 * 1); // adjust size as needed
   const receivedData = [];
   let bytesRead;
 
   /* eslint-disable no-await-in-loop */
   try {
     do {
-      bytesRead = await readWithTimeout(pushStream, dataBuffer, 5000); // 5 second timeout
+      bytesRead = await readWithTimeout(pushStream, dataBuffer, 1000 * 1); // 1 second timeout
       receivedData.push(new Uint8Array(dataBuffer.slice(0, bytesRead)));
     } while (bytesRead > 0);
   } catch (err) {
@@ -62,39 +62,45 @@ const readAllFromStream = async (pushStream) => {
 };
 
 export const requestToAzure = (synthesizer, pushStream) => {
-  Clipboard.read((response) => {
-    const text = response.data;
-    if (text === "") {
-      console.error("Clipboard is empty");
-      return;
-    }
+  return new Promise((_resolve, reject) => {
+    Clipboard.read((response) => {
+      const text = response.data;
+      if (text === "") {
+        console.error("Clipboard is empty");
+        reject("Clipboard is empty");
+        return;
+      }
+      synthesizer.speakTextAsync(
+        text,
+        async (result) => {
+          if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+            try {
+              // Convert the push stream to a blob and create a URL for it
+              const audioBlob = await readAllFromStream(pushStream);
+              const audioUrl = URL.createObjectURL(audioBlob);
 
-    synthesizer.speakTextAsync(
-      text,
-      async (result) => {
-        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-          // Convert the push stream to a blob and create a URL for it
-          const audioBlob = await readAllFromStream(pushStream);
-          const audioUrl = URL.createObjectURL(audioBlob);
-
-          // Create a link to download the file
-          const downloadLink = document.createElement("a");
-          downloadLink.href = audioUrl;
-          downloadLink.download = `${text}.mp3`;
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-          document.body.removeChild(downloadLink);
-        } else {
-          console.error(
-            `Speech synthesis canceled, ${result.errorDetails}\nDid you set the speech resource key and region values?`,
-          );
-        }
-        synthesizer.close();
-      },
-      (err) => {
-        console.trace(`err - ${err}`);
-        synthesizer.close();
-      },
-    );
+              // Create a link to download the file
+              const downloadLink = document.createElement("a");
+              downloadLink.href = audioUrl;
+              downloadLink.download = `${text}.mp3`;
+              document.body.appendChild(downloadLink);
+              downloadLink.click();
+              document.body.removeChild(downloadLink);
+            } catch (err) {
+              console.error(`Error during audio processing: ${err}`);
+            }
+          } else {
+            console.error(
+              `Speech synthesis canceled, ${result.errorDetails}\nDid you set the speech resource key and region values?`,
+            );
+          }
+          synthesizer.close();
+        },
+        (err) => {
+          console.trace(`err - ${err}`);
+          synthesizer.close();
+        },
+      );
+    });
   });
 };
