04a3706b306ef12ce3f50a95ce25078534141231
uuta <nightrage.metal66@gmail.com>
2023-06-25 15:05:53 +0900

    Added the speech_voice key into conf.priv.example.js

--- src/conf.priv.example.js
+++ src/conf.priv.example.js
@@ -39,6 +39,7 @@ const keys = {
   // https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/get-started-text-to-speech?pivots=programming-language-javascript&tabs=macos%2Cterminal
   speech_key: "",
   speech_region: "",
+  speech_voice: "",
 
   // ****** GitHub ****** //
   github_org: "",
